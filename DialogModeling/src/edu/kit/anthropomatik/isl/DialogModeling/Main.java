56e29bb932018608561d0d2b8a9c41379d9da5e8
ukcvo <ukcvo@student.kit.edu>
2014-04-30 21:34:05 +0200

    added openCV jar and references

--- DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/Main.java
+++ DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/Main.java
@@ -1,8 +1,12 @@
 package edu.kit.anthropomatik.isl.DialogModeling;
 
+import org.opencv.core.Core;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		// TODO Auto-generated method stub
 		System.out.println("Hello Lucas2");
 	}
