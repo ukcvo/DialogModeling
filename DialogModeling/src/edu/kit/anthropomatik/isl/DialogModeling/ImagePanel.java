df293e52dbedca998c9c9a700f1cbf0ea6ce8378
ukcvo <ukcvo@student.kit.edu>
2014-05-01 12:29:22 +0200

    moved video logic into ImagePanel

--- DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/ImagePanel.java
+++ DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/ImagePanel.java
@@ -2,8 +2,18 @@ package edu.kit.anthropomatik.isl.DialogModeling;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
 
 public class ImagePanel extends JPanel {
 
@@ -11,8 +21,11 @@ public class ImagePanel extends JPanel {
 
 	private BufferedImage image;
 	
-	public ImagePanel() {
+	private VideoCapture camera;
+	
+	public ImagePanel(VideoCapture camera) {
 		super();
+		this.camera = camera;
 		image = new BufferedImage(100, 100, BufferedImage.TYPE_3BYTE_BGR);
 	}
 	
@@ -24,7 +37,34 @@ public class ImagePanel extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
+		Mat frame = new Mat();
+		camera.read(frame);
+		Mat original = frame.clone(); // clone is mandatory!
+		
+		// TODO: Lucas --> face detection code
+		
+		// display result
+		showResult(original);
+		System.out.println("beep");
+		
 		g.drawImage(image, 0, 0, null);
+	}
+	
+	private void showResult(Mat img) {
+		Imgproc.resize(img, img, new Size(640, 480));
+	    MatOfByte matOfByte = new MatOfByte();
+	    Highgui.imencode(".jpg", img, matOfByte);
+	    byte[] byteArray = matOfByte.toArray();
+	    BufferedImage bufImage = null;
+	    try {
+	        InputStream in = new ByteArrayInputStream(byteArray);
+	        bufImage = ImageIO.read(in);
+	        setImage(bufImage);
+	        
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
 	}
 	
 }
--- DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/Main.java
+++ DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/Main.java
@@ -1,27 +1,16 @@
 package edu.kit.anthropomatik.isl.DialogModeling;
 
 import java.awt.Dimension;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
-import org.opencv.imgproc.Imgproc;
 
 public class Main {
 
-	private static JFrame window;
-	private static ImagePanel image;
+	private static JFrame window; 		// main window
+	private static ImagePanel image;	// will display our video stream
 	
 	public static void main(String[] args) {
 		
@@ -39,41 +28,9 @@ public class Main {
 		window = new JFrame("DialogModeling");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setMinimumSize(new Dimension(640, 480));
-		image = new ImagePanel();
+		image = new ImagePanel(camera); 
 		window.getContentPane().add(image);
 		window.pack();
 		window.setVisible(true);
-		
-		int i = 0;
-		while (true) {
-			// grab next frame from camera
-			Mat frame = new Mat();
-			camera.read(frame);
-			Mat original = frame.clone(); // clone is mandatory!
-			
-			// TODO: Lucas --> face detection code
-			
-			// display result
-			showResult(original);
-			System.out.println(i++);
-		}
-		
 	}
-	
-	private static void showResult(Mat img) {
-		Imgproc.resize(img, img, new Size(640, 480));
-	    MatOfByte matOfByte = new MatOfByte();
-	    Highgui.imencode(".jpg", img, matOfByte);
-	    byte[] byteArray = matOfByte.toArray();
-	    BufferedImage bufImage = null;
-	    try {
-	        InputStream in = new ByteArrayInputStream(byteArray);
-	        bufImage = ImageIO.read(in);
-	        image.setImage(bufImage);
-	        
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	}
-
 }
