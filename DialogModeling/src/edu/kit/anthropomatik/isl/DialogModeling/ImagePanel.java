aeccea304a697ce72750c9f6f4234d116ac84277
ukcvo <ukcvo@student.kit.edu>
2014-05-01 13:06:32 +0200

    replaced image painting code (according to StackOverflow, this variant is faster)

--- DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/ImagePanel.java
+++ DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/ImagePanel.java
@@ -2,23 +2,16 @@ package edu.kit.anthropomatik.isl.DialogModeling;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
-import javax.imageio.ImageIO;
+import java.awt.image.DataBufferByte;
 import javax.swing.JPanel;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
-import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 public class ImagePanel extends JPanel {
@@ -63,20 +56,19 @@ public class ImagePanel extends JPanel {
 	
 	private void displayImage(Mat img) {
 		
-		Imgproc.resize(img, img, new Size(640, 480));
-	    MatOfByte matOfByte = new MatOfByte();
-	    Highgui.imencode(".jpg", img, matOfByte);
-	    byte[] byteArray = matOfByte.toArray();
-	    
-	    try {
-	    	
-	        InputStream in = new ByteArrayInputStream(byteArray);
-	        image = ImageIO.read(in);
-	        this.repaint();
-	        
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
+		int type = BufferedImage.TYPE_BYTE_GRAY;
+		if (img.channels() > 1)
+			type = BufferedImage.TYPE_3BYTE_BGR;
+		
+		int bufferSize = img.channels() * img.cols() * img.rows();
+		byte[] b = new byte[bufferSize];
+		img.get(0,0,b);
+		
+		image = new BufferedImage(img.cols(), img.rows(), type);
+		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(b, 0, targetPixels, 0, b.length);
+		
+		this.repaint();
 	}
 	
 }
