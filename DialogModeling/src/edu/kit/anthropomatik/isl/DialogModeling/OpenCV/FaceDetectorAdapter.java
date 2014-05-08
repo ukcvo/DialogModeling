3e60bdec08a8b669a8a0a52b331402f62b9e05d3
ukcvo <ukcvo@student.kit.edu>
2014-05-08 19:46:12 +0200

    using JavaCV-style face detection (might need some work to produce more stable results)

--- DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/OpenCV/FaceDetectorAdapter.java
+++ DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/OpenCV/FaceDetectorAdapter.java
@@ -3,13 +3,15 @@ package edu.kit.anthropomatik.isl.DialogModeling.OpenCV;
 import java.awt.image.BufferedImage;
 
 import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_core.CvMemStorage;
+import org.bytedeco.javacpp.opencv_core.CvSeq;
 import org.bytedeco.javacpp.opencv_core.IplImage;
-import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.Rect;
 import org.bytedeco.javacpp.opencv_highgui;
 import org.bytedeco.javacpp.opencv_highgui.CvCapture;
 import org.bytedeco.javacpp.opencv_imgproc;
-import org.bytedeco.javacpp.opencv_objdetect.CascadeClassifier;
+import org.bytedeco.javacpp.opencv_objdetect;
+import org.bytedeco.javacpp.opencv_objdetect.CvHaarClassifierCascade;
 
 import edu.kit.anthropomatik.isl.DialogModeling.Display.ImagePanel;
 
@@ -18,8 +20,9 @@ public class FaceDetectorAdapter {
 	private ImagePanel videoStreamPanel; 	//display the video stream
 	private ImagePanel facePanel;			//display the detected face
 	
-	private CvCapture camera;
-	private CascadeClassifier faceDetector;	
+	private CvCapture camera;				
+	private CvHaarClassifierCascade faceDetector;
+	private CvMemStorage storage;
 	
 	public FaceDetectorAdapter(ImagePanel videoStreamPanel, ImagePanel facePanel) {
 		this.videoStreamPanel = videoStreamPanel;
@@ -28,21 +31,27 @@ public class FaceDetectorAdapter {
 		// grab the camera
 		this.camera = opencv_highgui.cvCreateCameraCapture(0);
 		// this detector seems to be the most stable one
-		this.faceDetector = new CascadeClassifier("./resources/haarcascade_frontalface_alt_tree.xml");
+		this.faceDetector = new CvHaarClassifierCascade(opencv_core.cvLoad("./resources/haarcascade_frontalface_alt_tree.xml"));
+		this.storage = CvMemStorage.create();
 	}
 	
 	public void detectFaces() {
 		
 		// get new frame from camera
 		IplImage original = opencv_highgui.cvQueryFrame(camera);
-		Mat matOfOriginal = new Mat(original);
 		
 		// detect faces
 		Rect face = new Rect();
-		faceDetector.detectMultiScale(matOfOriginal, face);
-		// TODO Lucas: use other detector?
-		// TODO Lucas: CvHaarClassifierCascade cascade = new CvHaarClassifierCascade(cvLoad(CASCADE_FILE));
-
+		CvSeq myFaces = opencv_objdetect.cvHaarDetectObjects(original, faceDetector, storage, 1.1, 1, 0);
+		
+		for (int i = 0; i < myFaces.total(); i++) {
+			Rect r = new Rect(opencv_core.cvGetSeqElem(myFaces, i));
+			if (r.area() > face.area()) // pick largest face
+				face = r;
+			// put a rectangle into the video stream to highlight detected face
+			opencv_core.cvRectangle(original, opencv_core.cvPoint(r.x(), r.y()), opencv_core.cvPoint(r.x() + r.width(), r.y() + r.height()), opencv_core.cvScalar(0, 255, 0, 0));
+		}
+		
 		if (face.area() > 0) {	// we detected a face
 			
 			// cut it out			
@@ -60,25 +69,10 @@ public class FaceDetectorAdapter {
 			facePanel.setImage(faceFrame);
 		}
 		
-		// put a rectangle into the video stream to show detected face
-		opencv_core.cvRectangle(original, opencv_core.cvPoint(face.x(), face.y()), opencv_core.cvPoint(face.x() + face.width(), face.y() + face.height()), opencv_core.cvScalar(0, 255, 0, 0));
-		
 		// always display video stream
 		BufferedImage videoStreamFrame = new BufferedImage(640, 480, BufferedImage.TYPE_3BYTE_BGR);
 		original.copyTo(videoStreamFrame); //matToImage(original);
 		videoStreamPanel.setImage(videoStreamFrame);
 		
 	}
-	
-	// commented out for now, but might be useful in the near future; TODO Lucas: Reuse or delete
-	// returns the largest Rect in the given MatOfRect
-//	private Rect getLargestFace(MatOfRect facesList) {
-//		Rect result = new Rect();
-//		for(Rect rect : facesList.toArray()) {
-//			if (rect.area() > result.area()) {
-//				result = rect;
-//			}
-//		}
-//		return result;
-//	}
 }
