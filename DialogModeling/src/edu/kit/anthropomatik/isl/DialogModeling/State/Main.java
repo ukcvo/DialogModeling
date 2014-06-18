3492f462c54b68bb33520831c1c27422d626fbb2
Andre Scheurich <uoai@student.kit.edu>
2014-06-18 11:58:50 +0200

    Initial complete State Machine

--- DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/State/Main.java
+++ DialogModeling/src/edu/kit/anthropomatik/isl/DialogModeling/State/Main.java
@@ -6,13 +6,15 @@ import org.customsoft.stateless4j.delegates.Action;
 public class Main {
 
 	public enum State {
-		IDLE, RECOGNIZE_USER, GREET_USER, ASK_USER_FOR_NAME, SMALLTALK
+		IDLE, RECOGNIZE_USER, SELF_TALK, GREET_USER, ASK_FOR_NAME, SMALL_TALK, 
+		COLLECT_USER_DATA, ASK_FOR_HELP, INSULT_USER, WAITING_FOR_ELEVATOR,
+		SAY_GOODBYE
 	}
 	
 	public enum Trigger {
-		HEARED_USER, SEEN_USER, 
-		USER_RECOGNIZED, USER_NOT_RECOGNIZED, 
-		USER_GREETS, USER_BUSY
+		NO_FACE_DETECTED, FACE_DETECTED, USER_RECOGNIZED, USER_NOT_RECOGNIZED,
+		USER_UNKNOWN, NOT_UNDERSTOOD, USER_GREETS, WANT_HELP, USER_HELPING, USER_NOT_HELPING,
+		JOB_DONE, DIALOG_DONE, SELF_TALK_OVER
 	}
 
 	public static void main(String[] args) {
@@ -22,10 +24,25 @@ public class Main {
 				checkMultimodalInput();
 			}
 		};
+		
+		Action callSelfTalk = new Action() {
+			@Override
+			public void doIt() {
+				doSelfTalk();
+			}
+		};
+		
 		Action callUserRecognition = new Action() {
 			@Override
 			public void doIt() {
 				recognizeUser();
+			}
+		};
+		
+		Action callNameRecognition = new Action() {
+			@Override
+			public void doIt() {
+				recognizeName();
 			}
 		};
 		
@@ -48,24 +65,53 @@ public class Main {
 		try {
 			robotInteraction.Configure(State.IDLE)
 			.OnEntry(callMultiModalInputCheck)
-			.Permit(Trigger.HEARED_USER, State.RECOGNIZE_USER)
-			.Permit(Trigger.SEEN_USER, State.RECOGNIZE_USER);
+			.Permit(Trigger.NO_FACE_DETECTED, State.SELF_TALK)
+			.Permit(Trigger.FACE_DETECTED, State.RECOGNIZE_USER);
+			
+			robotInteraction.Configure(State.SELF_TALK)
+			.OnEntry(callMultiModalInputCheck)
+			.Permit(Trigger.SELF_TALK_OVER, State.IDLE);
 						
 			robotInteraction.Configure(State.RECOGNIZE_USER)
 			.OnEntry(callUserRecognition)
 			.Permit(Trigger.USER_RECOGNIZED, State.GREET_USER)
-			.Permit(Trigger.USER_NOT_RECOGNIZED, State.ASK_USER_FOR_NAME);
+			.Permit(Trigger.USER_NOT_RECOGNIZED, State.ASK_FOR_NAME);
 
+			robotInteraction.Configure(State.ASK_FOR_NAME)
+			.OnEntry(callNameRecognition)
+			.Permit(Trigger.USER_RECOGNIZED, State.GREET_USER)
+			.Permit(Trigger.USER_UNKNOWN, State.COLLECT_USER_DATA);
+			
+			robotInteraction.Configure(State.COLLECT_USER_DATA)
+			.OnEntry(callNameRecognition)
+			.Permit(Trigger.USER_RECOGNIZED, State.GREET_USER);
+			
 			robotInteraction.Configure(State.GREET_USER)
 			.OnEntry(callUserInteraction)
-			.OnExit(callThisAndThat)
-			.Permit(Trigger.USER_GREETS, State.SMALLTALK)
-			.Permit(Trigger.USER_BUSY, State.IDLE);
+			.Permit(Trigger.USER_GREETS, State.SMALL_TALK);
 			
-			//...
+			robotInteraction.Configure(State.SMALL_TALK)
+			.OnEntry(callNameRecognition)
+			.Permit(Trigger.WANT_HELP, State.ASK_FOR_HELP);
 			
+			robotInteraction.Configure(State.ASK_FOR_HELP)
+			.OnEntry(callNameRecognition)
+			.Permit(Trigger.USER_HELPING, State.WAITING_FOR_ELEVATOR)
+			.Permit(Trigger.USER_NOT_HELPING, State.SAY_GOODBYE);
 			
-			robotInteraction.Fire(Trigger.SEEN_USER);
+			robotInteraction.Configure(State.WAITING_FOR_ELEVATOR)
+			.OnEntry(callNameRecognition)
+			.Permit(Trigger.JOB_DONE, State.SAY_GOODBYE);
+			
+			robotInteraction.Configure(State.INSULT_USER)
+			.OnEntry(callNameRecognition)
+			.Permit(Trigger.JOB_DONE, State.SAY_GOODBYE);
+			
+			robotInteraction.Configure(State.SAY_GOODBYE)
+			.OnEntry(callNameRecognition)
+			.Permit(Trigger.DIALOG_DONE, State.IDLE);
+			
+			robotInteraction.Fire(Trigger.FACE_DETECTED);
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -74,6 +120,16 @@ public class Main {
 		
 		System.out.println("Current State: " +robotInteraction.getState());
 
+	}
+
+	protected static void recognizeName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected static void doSelfTalk() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	protected static void doThisAndThat() {
